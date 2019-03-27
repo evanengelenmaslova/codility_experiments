17efe91ce4d9dafcfc42313e710fb2ecba726f21
evanengelen <elena.van.engelen@telfortglasvezel.nl>
2019-03-27 14:38:44 +0100

    add TapeEquilibrium

---
+++ src/test/java/codility_experiments/time_complexity/TapeEquilibriumTest.java
@@ -0,0 +1,27 @@
+package codility_experiments.time_complexity;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TapeEquilibriumTest {
+
+	@Test
+	public void shouldReturn1ForExampleTestCase() {
+		  int[] A = {3,1,2,4,3};
+		  assertEquals(1,TapeEquilibrium.solution(A));
+	}
+	
+	@Test
+	public void shouldReturn0ForTestCaseWithNegatives() {
+		  int[] A = {-3,3,1,2,4,3,-4};
+		  assertEquals(0,TapeEquilibrium.solution(A));
+	}
+	
+	@Test
+	public void shouldReturn6ForTestCaseWith2Elements() {
+		  int[] A = {2,8};
+		  assertEquals(6,TapeEquilibrium.solution(A));
+	}
+
+}
