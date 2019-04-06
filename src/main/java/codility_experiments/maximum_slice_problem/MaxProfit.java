5110d71143210eb275e0bbd205f83d0c04420f2b
evanengelen <elena.van.engelen@telfortglasvezel.nl>
2019-04-06 15:26:27 +0200

    add max profit puzzle solution

---
+++ src/main/java/codility_experiments/maximum_slice_problem/MaxProfit.java
@@ -0,0 +1,29 @@
+package codility_experiments.maximum_slice_problem;
+
+/**
+ * See <a href=
+ * "https://app.codility.com/programmers/lessons/9-maximum_slice_problem/max_profit/">
+ * Max Profit</a>
+ * 
+ * @author evanengelen
+ *
+ */
+public class MaxProfit {
+
+	public static int solution(int[] A) {
+		int max = -1;
+		int maxProfit = -1;
+		// loop from end
+		// substruct from last, one before, if one before last is bigger, use
+		// that one
+		if (A != null && A.length > 1) {
+			for (int i = A.length - 1; i > 0; i--) {
+				max = Math.max(max, A[i]);
+				int profit = max - A[i - 1];
+				maxProfit = Math.max(maxProfit, profit);
+			}
+		}
+		return maxProfit > 0 ? maxProfit : 0;
+	}
+
+}
